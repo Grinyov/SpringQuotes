0673e52f7b6da75c6e69bc608c98760a63e7e3a3
green <vgrinyov@gmail.com>
2015-09-28 18:32:53 +0300

    add  new functionally(screensaver)

---
+++ src/main/java/screensaver/ColorFrame.java
@@ -0,0 +1,26 @@
+package screensaver;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Random;
+
+/**
+ * Created by Grinyov Vitaliy on 28.09.15.
+ */
+public class ColorFrame extends JFrame {
+    @Autowired
+    private Color color;
+
+    public ColorFrame(){
+        setSize(200, 200);
+        setVisible(true);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+
+    public void showOnRandomPlace(){
+        Random random = new Random();
+        setLocation(random.nextInt(1200), random.nextInt(700));
+    }
+}
